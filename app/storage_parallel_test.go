ffb4f2f401083743394fa84de0291b384fbdb1d7
Joubert RedRat <eu+git@redrat.com.br>
2021-06-06 22:15:47 -0300

    add parallel test

---
+++ app/storage_parallel_test.go
@@ -0,0 +1,57 @@
+package app_test
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/joubertredrat/pokelang/app"
+)
+
+func TestStorageTableParallel(t *testing.T) {
+	tests := []struct {
+		name          string
+		storage       app.Storage
+		countExpected int
+	}{
+		{
+			name:          "Test storage with 10 items",
+			storage:       app.NewMemoryStorage(),
+			countExpected: 10,
+		},
+		{
+			name:          "Test storage with 50 items",
+			storage:       app.NewMemoryStorage(),
+			countExpected: 50,
+		},
+		{
+			name:          "Test storage with 100 items",
+			storage:       app.NewMemoryStorage(),
+			countExpected: 100,
+		},
+		{
+			name:          "Test storage with random items",
+			storage:       app.NewMemoryStorage(),
+			countExpected: rand.Intn(499-10) + 499,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			storage := test.storage
+
+			if 0 != storage.Count() {
+				t.Errorf("storage.Count() expected 0, got %d", storage.Count())
+			}
+
+			for i := 1; i <= test.countExpected; i++ {
+				storage.AddOne()
+			}
+
+			if uint(test.countExpected) != storage.Count() {
+				t.Errorf("storage.Count() expected %d, got %d", test.countExpected, storage.Count())
+			}
+		})
+	}
+}
